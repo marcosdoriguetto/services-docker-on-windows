7efad016ceb3fa9e31a86c766453eddbc7c68009
Marcos Fernandes <marcosdoriguetto@gmail.com>
2022-01-10 01:20:04 -0300

    Refaração e adição da opção de reinicialização do Windows

--- scripts.js
+++ scripts.js
@@ -1,23 +1,37 @@
 const Shell = require('node-powershell')
 
-const runAsAdmin = async (command) => {
-  const usePowerShell = typeof command === 'string'
-  const shell = new Shell({})
-  await shell.addCommand('Start-Process')
+const getValueCheckBox = () => document.querySelector('#restart').checked;
 
-  if (usePowerShell) await shell.addArgument('PowerShell')
-  await shell.addArgument('-Verb')
-  await shell.addArgument('RunAs')
-  await shell.addArgument('-PassThru')
-  if (usePowerShell) await shell.addArgument('-Wait')
+function activeResource() {
+  const valueCheckBox = getValueCheckBox();
+
+  runAsAdmin(`"${linuxActive}; ${machineActive}; ${hyperActive}; ${valueCheckBox && restartComputer}"`);
+}
+
+function disableResource() {
+  const valueCheckBox = getValueCheckBox();
+
+  runAsAdmin(`"${linuxDisable}; ${machineDisable}; ${hyperDisable};${valueCheckBox && restartComputer}"`);
+}
+
+const runAsAdmin = async (command) => {
+  const usePowerShell = typeof command === 'string';
+  const shell = new Shell({});
+  await shell.addCommand('Start-Process');
+
+  if (usePowerShell) await shell.addArgument('PowerShell');
+  await shell.addArgument('-Verb');
+  await shell.addArgument('RunAs');
+  await shell.addArgument('-PassThru');
+  if (usePowerShell) await shell.addArgument('-Wait');
 
   if (usePowerShell) {
-    await shell.addArgument('-ArgumentList')
-    await shell.addArgument(command)
-  }
+    await shell.addArgument('-ArgumentList');
+    await shell.addArgument(command);
+  };
 
-  await shell.invoke()
-  return await shell.dispose()
+  await shell.invoke();
+  return await shell.dispose();
 }
 
 const hyperActive = "dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V /all /norestart";
@@ -27,7 +41,10 @@ const linuxDisable = "dism.exe /online /disable-feature /featurename:Microsoft-W
 const machineDisable = "dism.exe /online /disable-feature /featurename:VirtualMachinePlatform /norestart";
 const hyperDisable = "dism.exe /online /disable-feature /featurename:Microsoft-Hyper-V /norestart";
 
+const restartComputer = "shutdown /r -t 0";
+
 const buttonActive = document.querySelector('.active');
-buttonActive.onclick = () => runAsAdmin(`"${linuxActive}; ${machineActive}; ${hyperActive}"`);
+buttonActive.onclick = () => activeResource();
 const buttonDisable = document.querySelector('.disable');
-buttonDisable.onclick = () => runAsAdmin(`"${linuxDisable}; ${machineDisable}; ${hyperDisable}"`);
+buttonDisable.onclick = () => disableResource();
+
